f77d5dcfa0a715d86d8a4bf6a0946d07609fff5b
Lewis Peckover <lew@lew.io>
2017-02-20 18:29:10 +0800

    unsupported types should be a fatal error

--- json.go
+++ json.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/antonholmquist/jason"
 	"io"
+	"os"
 	"strings"
 )
 
@@ -20,24 +21,26 @@ func parseJson(fp io.Reader) error {
 
 func jsonWalk(prefix []string, obj *jason.Object, err error) error {
 	for k, v := range obj.Map() {
+		key := strings.Join(append(prefix, k), "/")
+		Trace.Printf("JSON iteration: %s", key)
 		switch v.Interface().(type) {
 		case string:
-			Info.Printf("%v: %v\n", strings.Join(append(prefix, k), "/"), v.Interface())
+			Info.Printf("%v: %v\n", key, v.Interface())
 			enc.Encode(v.Interface())
 		case json.Number:
-			Info.Printf("%v: %v\n", strings.Join(append(prefix, k), "/"), v.Interface())
+			Info.Printf("%v: %v\n", key, v.Interface())
 			enc.Encode(v.Interface())
 		case []interface{}:
 			// json array
 			o, _ := v.Array()
-			Info.Printf("%v: %v\n", strings.Join(append(prefix, k), "/"), strings.Join(jsonArrayChoose(o), ", "))
+			Info.Printf("%v: %v\n", key, strings.Join(jsonArrayChoose(o), ", "))
 			enc.Encode(v.Interface())
 		case map[string]interface{}:
 			// json object
 			o, _ := v.Object()
 			jsonWalk(append(prefix, k), o, err)
 		case bool:
-			Info.Printf("%v: %v\n", strings.Join(append(prefix, k), "/"), v.Interface())
+			Info.Printf("%v: %v\n", key, v.Interface())
 			enc.Encode(v.Interface())
 		case nil:
 			// json nulls
@@ -58,7 +61,8 @@ func jsonArrayChoose(arr []*jason.Value) (ret []string) {
 		case bool:
 			ret = append(ret, fmt.Sprintf("%v", v.Interface()))
 		default:
-			Warning.Printf("Ignoring type %T in array. Only strings, numbers and boolean values are supported.\n", v.Interface())
+			Error.Printf("Invalid type %T in array. Only strings, numbers and boolean values are supported.\n", v.Interface())
+			os.Exit(1)
 		}
 	}
 	return ret
