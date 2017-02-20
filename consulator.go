e2b6c093c466594fe9eb3235b20a14e22268fab8
Lewis Peckover <lew@lew.io>
2017-02-20 17:28:28 +0800

    sort out logging

--- consulator.go
+++ consulator.go
@@ -1,60 +1,99 @@
 package main
 
 import (
-  "os"
-  "io"
-  "flag"
-  "fmt"
-  "strings"
-  "log"
-  "path/filepath"
-  "encoding/json"
+	"encoding/json"
+	"flag"
+	//"fmt"
+	"github.com/fatih/color"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-var path = flag.String("path", "", "Path to file or directory containing data to import")
-var enc = json.NewEncoder(os.Stdout)
+var (
+	path    = flag.String("path", "", "Path to file or directory containing data to import")
+	debug   = flag.Bool("debug", false, "Show debugging information")
+	trace   = flag.Bool("trace", false, "Show even more debugging information")
+	enc     = json.NewEncoder(os.Stdout)
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func logInit() {
+	if *trace {
+		Trace = log.New(os.Stderr, "TRACE: ", 0)
+		Info = log.New(os.Stderr, color.BlueString("INFO: "), 0)
+	} else if *debug {
+		Trace = log.New(ioutil.Discard, "TRACE: ", 0)
+		Info = log.New(os.Stderr, color.BlueString("INFO: "), 0)
+
+	} else {
+		Trace = log.New(ioutil.Discard, "TRACE: ", 0)
+		Info = log.New(ioutil.Discard, color.BlueString("INFO: "), 0)
+	}
+	Warning = log.New(os.Stderr, color.RedString("WARNING: "), 0)
+	Error = log.New(os.Stderr, color.RedString("ERROR: "), 0)
+}
 
 func main() {
-  flag.Parse()
-  _, err := os.Stat(*path)
-  if err != nil {
-    log.Fatal(err)
-  }
-  err = filepath.Walk(*path, parseConfig)
-  if err != nil {
-    log.Fatal(err)
-  }
+	flag.Parse()
+	logInit()
+	_, err := os.Stat(*path)
+	if err != nil {
+		Error.Fatal(err)
+	}
+	err = filepath.Walk(*path, parseConfig)
+	if err != nil {
+		Error.Fatal(err)
+	}
+	Dump()
 }
 
 func parseConfig(path string, f os.FileInfo, err error) error {
-  if err != nil { log.Printf("%v: %v\n", path, err) }
-  if f.Mode().IsRegular() {
-    fp, err := os.Open(path)
-    if err != nil { log.Printf("%v: %v\n", path, err) }
-    switch {
-      case strings.HasSuffix(path, ".json"):
-        fmt.Println("json file found!")
-        err := parseJson(fp)
-        if err != nil { log.Printf("%v: %v\n", path, err) }
-      case strings.HasSuffix(path, ".yml"):
-        fallthrough
-      case strings.HasSuffix(path, ".yaml"):
-        fmt.Println("found a yaml file!")
-        // yaml handling based on https://github.com/bronze1man/yaml2json
-        yamlR, yamlW := io.Pipe()
-        go func() {
-          defer yamlW.Close()
-          err := yamlToJson(fp, yamlW)
-          if err != nil { log.Printf("%v: %v\n") }
-        }()
-        err := parseJson(yamlR)
-        if err != nil { log.Printf("%v: %v\n", path, err) }
-     case strings.HasSuffix(path, ".properties"):
-        fmt.Println("found a properties file!")
-      case strings.HasSuffix(path, ".ini"):
-        fmt.Println("found an ini file!")
-      default:
-    }
-  }
-  return nil
+	Trace.Printf("Traversing %s", path)
+	if err != nil {
+		Warning.Printf("%v: %v\n", path, err)
+	}
+	if f.Mode().IsRegular() {
+		fp, err := os.Open(path)
+		if err != nil {
+			Warning.Printf("%v: %v\n", path, err)
+		}
+		switch {
+		case strings.HasSuffix(path, ".json"):
+			Info.Printf("Parsing %s as json", path)
+			err := parseJson(fp)
+			if err != nil {
+				Warning.Printf("%v: %v\n", path, err)
+			}
+		case strings.HasSuffix(path, ".yml"):
+			fallthrough
+		case strings.HasSuffix(path, ".yaml"):
+			Info.Printf("Parsing %s as yaml", path)
+			// yaml handling based on https://github.com/bronze1man/yaml2json
+			yamlR, yamlW := io.Pipe()
+			go func() {
+				defer yamlW.Close()
+				err := yamlToJson(fp, yamlW)
+				if err != nil {
+					Warning.Printf("%v: %v\n", path, err)
+				}
+			}()
+			err := parseJson(yamlR)
+			if err != nil {
+				Warning.Printf("%v: %v\n", path, err)
+			}
+		case strings.HasSuffix(path, ".properties"):
+			Info.Printf("Parsing %s as properties", path)
+		case strings.HasSuffix(path, ".ini"):
+			Info.Printf("Parsing %s as ini", path)
+		default:
+		}
+	}
+	return nil
 }
