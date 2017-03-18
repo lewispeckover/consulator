4f1e4ab4d8531757d61d53ef79af173fa83b3e2e
Lewis Peckover <lew@lew.io>
2017-03-18 06:06:54 +0000

    trim trailing linebreak on raw files

--- command/configparser/raw.go
+++ command/configparser/raw.go
@@ -9,7 +9,7 @@ import (
 func parseRaw(fp io.Reader, prefix []string, glue string) error {
 	contents, err := ioutil.ReadAll(fp)
 	if err == nil {
-		data[strings.Join(prefix, "/")] = contents
+		data[strings.Join(prefix, "/")] = []byte(strings.TrimSuffix(string(contents), "\n"))
 	}
 	return err
 }
